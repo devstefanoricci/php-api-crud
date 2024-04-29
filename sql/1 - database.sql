dce7ab080e9b20310e7a5c41f26a1b1000d31588
Stefano Ricci <stefanoricci.eu@gmail.com>
2024-04-29 12:00:05 +0200

    typo

--- sql/1 - database.sql
+++ sql/1 - database.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `employees` (
 `id` int(11) NOT NULL AUTO_INCREMENT,
 `name` varchar(256) NOT NULL,
 `email` varchar(50),
-`designation` varchar(255) NOT NULL,
-`created_At` datetime NOT NULL,
+`area` varchar(255) NOT NULL,
+`created_at` datetime NOT NULL,
 PRIMARY KEY (`id`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=19;
